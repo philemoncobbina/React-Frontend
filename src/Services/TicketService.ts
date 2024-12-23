79191185101b49a74f639fc165e030eb1698312d
philemoncobbina <philemoncobbina19@gmail.com>
2024-12-23 23:18:21 +0000

    Your commit message describing the changes

--- src/Services/TicketService.ts
+++ src/Services/TicketService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/tickets/'; // Replace with your actual API URL
+const API_URL = 'https://api.plvcmonline.uk/api/tickets/'; // Replace with your actual API URL
 
 export interface TicketData {
     full_name: string;
