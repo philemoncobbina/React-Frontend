285a3b080205577e4a91907f929e79348dd01122
philemoncobbina <philemoncobbina19@gmail.com>
2025-03-09 18:16:09 +0000

    Updated feature X

--- src/Services/jobService.ts
+++ src/Services/jobService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/api/';
+const API_BASE_URL = 'https://api.plvcmonline.uk/api/';
 
 export interface JobPost {
   id: number;
